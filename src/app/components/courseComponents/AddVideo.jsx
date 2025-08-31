fa5fa02a416d5aa963baf64f831f1ba61eb7c886
Anaesthesiaacademye <anaesthesiaacademy1@gmail.com>
2025-08-31 19:43:29 +0300

    submit text edited

--- src/app/components/courseComponents/AddVideo.jsx
+++ src/app/components/courseComponents/AddVideo.jsx
@@ -155,7 +155,7 @@ export default function AddVideo({ courses, queryClient, selectedCourse }) {
         className="bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
         type="submit"
       >
-        Submit Course
+        Submit Video
       </PrimaryButton>
     </form>
   );
